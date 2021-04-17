0caa8b49f06540c803fdc194d8798c5b345dc147
Cornelia Stordalen <cornelia@sfjbb.net>
2021-04-17 15:26:52 +0200

    Added Food.java and FoodType.java

--- Eksamen/src/Food.java
+++ Eksamen/src/Food.java
@@ -1,2 +1,12 @@
 public class Food {
+    String name;
+    boolean isVegan;
+    float caloriesPerDay;
+    // Konstruktør
+    Food(String name, boolean isVegan, float caloriesPerDay){
+        this.name = name;
+        this.isVegan = isVegan;
+        this.caloriesPerDay = caloriesPerDay;
+    }
 }
+
--- Eksamen/src/FoodType.java
+++ Eksamen/src/FoodType.java
@@ -1,4 +1,18 @@
 package PACKAGE_NAME;
 
-public class FoodType {
+public class FoodType extends Food {
+
+    //Vil ikke ha med enumen??
+    FoodType(String name, boolean isVegan, float caloriesPerDay) {
+        super(name, isVegan, caloriesPerDay);
+        this.type = type;
+
+    }
+
+}
+enum type {
+    CARB,
+    FAT,
+    PROTEIN,
+    RECIPE;
 }
