bee75b893cebd9a7e17c77cda65db2e287a48722
Cornelia Stordalen <cornelia@sfjbb.net>
2021-04-24 16:49:12 +0200

    LowCarbDiet

--- Eksamen/src/LowCarbDiet.java
+++ Eksamen/src/LowCarbDiet.java
@@ -1,21 +1,37 @@
 public class LowCarbDiet extends Diet {
-
-
+    // Skal man sette en vekt som er minimum?
     Float minWeightKg;
 
-//    public LowCarbDiet(int daysDuration, String purpose, Food[] allowedFood, boolean isVegan, Float minWeightKg) {
-//        super(daysDuration, purpose, allowedFood, isVegan);
-//        this.minWeightKg = minWeightKg;
-//    }
+    public LowCarbDiet(String name) {
+        super.setName(name);
+    }
 
-//
-//    @Override
-//    public String writeDuration() {
-//        return null;
-//    }
-//
-//    @Override
-//    public String writeAllowedFood() {
-//        return null;
-//    }
+    // This diet can contain maximum two Meals with foodtype Carb
+    // Array
+    Food[] LowCarbDietFood = new Food[2];
+    LowCarbDietFood[0] = new Food("Lasagna", false);
+    LowCarbDietFood[1] = new Food("Pizza", false);
+
+
+
+
+    //Skal følgende være på alle diettyper?
+    /*IF THE DIET LASTS FOR ONLY DAYS*/
+    public LowCarbDiet(String name, int daysDuration) {
+        super.setName(name);
+        super.setDaysDuration(daysDuration);
+    }
+
+    /*IF THE DIET LASTS FOR ONLY DAYS AND MONTHS*/
+    public LowCarbDiet(String name, int daysDuration, int monthDuration) {
+        super.setName(name);
+        super.setDaysDuration(daysDuration);
+        super.setMonthDuration(monthDuration);
+    }
+
+    public void setDaysDuration(int duration) {
+        super.setDaysDuration(duration);
+    }
+
+
 }
--- Eksamen/test/LowCarbDietTest.java
+++ Eksamen/test/LowCarbDietTest.java
@@ -1,5 +1,10 @@
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LowCarbDietTest {
+    @Test
+    public void testReq1_e_a(){
 
+    }
 }
